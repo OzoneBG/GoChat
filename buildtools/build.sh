380cef42a449b26c2e3c1e0a543096b7fa558cb4
OzoneBG <muzunov875@gmail.com>
2019-02-21 17:09:58 +0200

    Update buildtools

--- buildtools/build.sh
+++ buildtools/build.sh
@@ -1,10 +1,14 @@
 cd ..
-echo "Building main executable"
+echo "Building main executable.."
 go build -ldflags="-s -w" -o build/chat.exe
 cd buildtools/
-echo "Compressing built executable"
+echo "Compressing built executable.."
 ./upx -9 -o ../build/chatCompressed.exe ../build/chat.exe
 cd ../build
+echo "Renaming executable.."
 rm chat.exe
 mv chatCompressed.exe chat.exe
+cd ..
+echo "Copying dependancies.."
+cp -a ./templates ./build
 read
